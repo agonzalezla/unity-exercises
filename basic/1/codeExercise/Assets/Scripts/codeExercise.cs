fe40452c3a8ec7b552c2565e1c7bbc71007f0cae
Alberto Gonzalez <56535471+agonzalezla@users.noreply.github.com>
2024-10-12 17:24:07 +0200

    [Exercise]: Create basic-1

---
+++ basic/1/codeExercise/Assets/Scripts/codeExercise.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class codeExercise : MonoBehaviour
+{
+    // Variables
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The speed is: " + speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
